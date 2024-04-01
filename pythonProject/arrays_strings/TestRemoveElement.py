14ae3d2531f9268a3463802e7c0f48a32a4fb387
tanya-savchuk <164957264+tanya-savchuk@users.noreply.github.com>
2024-04-01 17:01:32 -0500

    Update TestRemoveElement.py

--- pythonProject/arrays_strings/TestRemoveElement.py
+++ pythonProject/arrays_strings/TestRemoveElement.py
@@ -4,20 +4,31 @@ from remove_element import Solution
 class Tests(unittest.TestCase):    
 
     def test_remove_element_1(self):
+
         nums = [0, 1, 2, 2, 3, 0, 4, 2]
         val = 2
         actual = Solution.remove_element(nums,val)
+
         expected_n = 5
         expected_nums = [0,1,3,0,4]
-        self.assertEqual(actual,expected_n)
-        
+
+        self.assertEqual(actual,expected_n)        
+        self.assertEqual(nums[0:actual],expected_nums)
+
+    def test_remove_duplicates_1(self):
+
+        nums = [0,0,1,1,1,2,2,3,3,4]
+        actual = Solution.remove_duplicates(nums)
+
+        expected_n = 5
+        expected_nums = [0,1,2,3,4]
+
+        self.assertEqual(actual,expected_n)        
         self.assertEqual(nums[0:actual],expected_nums)
 
 
+
 if __name__ == '__main__':
+
     unittest.main(exit=False)
 
-    # nums = [0,0,1,1,1,2,2,3,3,4]
-    # n = remove_duplicates(nums)
-    # print('n = ', n)
-    # print('nums = ', nums[0:n])
