92e22bc7c298131e6de137d9fa5cf21cff00f947
tanya-savchuk <164957264+tanya-savchuk@users.noreply.github.com>
2024-04-06 18:19:11 -0500

    Create letter_combos_phone_num.py

---
+++ pythonProject/backtracking/letter_combos_phone_num.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        """
+        Medium
+
+        Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. 
+        Return the answer in any order.
+        A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+        0 <= digits.length <= 4
+        digits[i] is a digit in the range ['2', '9'].
+        """
+        return []
+    
+    if __name__ == __main__:
+        digits = "23"
+        res = ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
