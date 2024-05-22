4d636f55f5d4102a433933649250a4b1944dca43
tanya-savchuk <164957264+tanya-savchuk@users.noreply.github.com>
2024-05-22 14:33:48 -0500

    updated randomized queue

--- javaProject/dsa/queues/RandomizedQueue.java
+++ javaProject/dsa/queues/RandomizedQueue.java
@@ -6,11 +6,10 @@ import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
-   private static final int INIT_CAPACITY = 8;
+    private static final int INIT_CAPACITY = 8;
 
-   private Item[] a; //array of items
+    private Item[] a; // array of items
     private int n; // number of elements in the queue
-
 
 
     // construct an empty randomized queue
@@ -30,28 +29,38 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }
 
     private void resize(int capacity) {
+        assert capacity >= n;
 
-        // alternative implementation
-         a = java.util.Arrays.copyOf(a, capacity);
+        // manual copy to pass the assignment
+        // normally would use this instead:
+        // a = java.util.Arrays.copyOf(a, capacity);
+        Item[] copy = (Item[]) new Object[capacity];
+        for (int i = 0; i < n; i++) {
+            copy[i] = a[i];
+        }
+        a = copy;
+
     }
 
 
     // add the item
     public void enqueue(Item item) {
-        if (n == a.length) resize(2*a.length);
+        if (item == null) throw new IllegalArgumentException("cannot enqueue null");
+        if (n == a.length) resize(2 * a.length);
         a[n++] = item;
 
     }
 
     // remove and return a random item
     public Item dequeue() {
+        if (isEmpty()) throw new NoSuchElementException("cannot dequeue from empty");
         int idx = StdRandom.uniformInt(n);
         Item item = a[idx];
 
         // swap move item at the end into idx slot
         a[idx] = a[--n];
         a[n] = null;
-        if (n <= a.length/4) resize(a.length/2);
+        if (n <= a.length / 4) resize(a.length / 2);
         return item;
 
     }
@@ -59,9 +68,19 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     // return a random item (but do not remove it)
     public Item sample() {
+        if (isEmpty()) throw new NoSuchElementException("cannot sample from empty");
         int idx = StdRandom.uniformInt(n);
         return a[idx];
 
+    }
+
+    private int[] shuffleIdx() {
+        int [] indices = new int[n];
+        for (int i = 0; i < n; i++) {
+            indices[i] = i;
+        }
+        StdRandom.shuffle(indices);
+        return indices;
     }
 
     // return an independent iterator over items in random order
@@ -71,14 +90,20 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     private class RandomizedQueueIterator implements Iterator<Item> {
 
+        private int k = 0;
+        int [] randIdx = shuffleIdx();
+
         @Override
         public boolean hasNext() {
-            return n > 0;
+            return k < n;
         }
 
         @Override
         public Item next() {
-            return sample();
+            if (!hasNext()) throw new NoSuchElementException("no more elements to return");
+            Item item = a[randIdx[k]];
+            k++;
+            return item;
         }
 
         @Override
@@ -92,10 +117,21 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     public static void main(String[] args) {
 
         RandomizedQueue<Integer> queue = new RandomizedQueue<>();
+        queue.enqueue(0);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        Iterator<Integer> iterator = queue.iterator();
+        int res;
+        while (iterator.hasNext()) {
+            res = iterator.next();
+            StdOut.println(res);
+        }
+
+        queue = new RandomizedQueue<>();
         queue.enqueue(380);
         queue.enqueue(298);
         queue.enqueue(722);
-        Iterator<Integer> iterator = queue.iterator();
+        iterator = queue.iterator();
         StdOut.println(iterator.hasNext());
         StdOut.println(iterator.next());
 
@@ -109,7 +145,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         StdOut.println("size " + rq.size());
 
         while (rq.size() > 0) {
-            int res = rq.dequeue();
+            res = rq.dequeue();
             StdOut.println("dequeue " + res);
         }
 
